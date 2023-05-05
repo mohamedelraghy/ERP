e04ca6ba3e6939d6a815d74514e50824ce8f1a09
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-06 00:03:09 +0300

    add login validation

--- validation/employee.js
+++ validation/employee.js
@@ -5,10 +5,20 @@ const validateEmployee = employee => {
     name: Joi.string().min(5).max(30).required(),
     email: Joi.string().min(5).max(255).email().required(),
     password: Joi.string().min(8).max(255).required(),
+    confirm_password: Joi.string().min(8).max(255).required(),
     role: Joi.string().valid('super_admin', 'hr', 'developer').required(),
-    salary: Joi.number().positive().default(0),
+    salary: Joi.number().positive().default(0).optional(),
   });
   return schema.validate(employee); 
 }
 
-module.exports = validateEmployee;
+const validateLoginEmployee = employee => {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(8).max(255).required(),
+  });
+  return schema.validate(employee); 
+}
+
+exports.validateEmp = validateEmployee;
+exports.validateLoginEmp = validateLoginEmployee;
