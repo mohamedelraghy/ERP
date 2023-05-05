cae20faa5ba42d6bbb809ac3bf8e2fb5f3cc7c94
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-06 00:03:39 +0300

    add login controller

---
+++ controllers/employee/index.js
@@ -0,0 +1,7 @@
+const signup = require('./sginup');
+const login = require('./login');
+
+module.exports = {
+  signup,
+  login
+}
---
+++ controllers/employee/login.js
@@ -0,0 +1,62 @@
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const prisma = new PrismaClient()
+const { validateLoginEmp } = require('../../validation/employee');
+
+module.exports = async(req, res, next) => {
+  const { error } = validateLoginEmp(req.body);
+  if (error) {
+    const err = new Error(error.details[0].message);
+    error.statusCode = 422;
+    return next(err);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  try {
+
+    const employee = await prisma.employee.findUnique({
+      where: {
+        email: email
+      }
+    });
+
+    if (!employee) {
+      const error = new Error('Employee with this email could not be found.');
+      error.statusCode = 401;
+      throw error;
+    }
+  
+    const isEqual = await bcrypt.compare(password, employee.password);
+    if (!isEqual) {
+      const error = new Error('Invalid email or password');
+      error.statusCode = 401;
+      throw error;
+    }
+    
+    const token = jwt.sign({
+        email: employee.email,
+        empId: employee.id.toString()
+      },
+      process.env.JWT_KEY,
+      { expiresIn: '1h' }
+    );
+  
+    res.status(200).json({
+      message: "Empolyee logedIn",
+      token: token,
+      employee: {
+        id: employee.id,
+        name: employee.name
+      }
+    });
+  } catch (err) {
+    if(!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+}
