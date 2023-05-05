a24ad5ec70b7cc911d922e1fda7f44d234783844
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-06 01:13:32 +0300

    add authintacation to creating employee

--- routes/employee.js
+++ routes/employee.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const employeeController = require('../controllers/employee');
+const auth = require('../middleware/is-Auth');
 
-router.post('/signup', employeeController.signup);
+router.post('/signup', auth, employeeController.signup);
 
 router.post('/login', employeeController.login);
 
