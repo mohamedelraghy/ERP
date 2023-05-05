a3e0789876cb3ac4c303af52208e918b89c07106
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-06 02:23:12 +0300

    return employee tasks

--- controllers/task/myTask.js
+++ controllers/task/myTask.js
@@ -1,8 +1,31 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client')
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 module.exports = async (req, res, next) => {
-
   
+  try {    
+    const tasks = await prisma.task.findMany({
+      where: {
+        employeeId: Number(req.empId),
+      }
+    });
+
+    if (!tasks.length) {
+      const error = new Error('No task found');
+      error.statusCode = 404;
+      throw error;
+    }
+      
+    res.status(200).json({ 
+      message: "tasks retrieved",
+      tasks: tasks
+    });
+
+  } catch (err) {
+    if (!err.status) {
+      err.status = 500;
+    }
+    next(err);
+  }
 }
