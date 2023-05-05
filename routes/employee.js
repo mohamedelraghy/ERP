11103e1f59cafbae38987a2b65e7267f30183760
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-06 00:02:45 +0300

    singup & login route

--- routes/employee.js
+++ routes/employee.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const employeeController = require('../controllers/employee');
 
+router.post('/signup', employeeController.signup);
+
+router.post('/login', employeeController.login);
 
 module.exports = router;
