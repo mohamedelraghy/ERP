aeb1fc3748d6da1e45734ed5fe91786bc8382c56
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-06 15:03:24 +0300

    add deadLine validation

--- validation/tasks.js
+++ validation/tasks.js
@@ -5,7 +5,12 @@ const validateTask = task => {
     title: Joi.string().min(5).max(30).required(),
     description: Joi.string().min(20).required(),
     status: Joi.string().valid('pending', 'in_progress', 'completed').default('pending').required(),
-    salary: Joi.number().positive().required()
+    salary: Joi.number().positive().required(),
+    deadLine: Joi.object({
+      day: Joi.number().positive().required(),
+      month: Joi.number().positive().required(),
+      year: Joi.number().positive().required()
+    }).required()
   });
   return schema.validate(task); 
 }
