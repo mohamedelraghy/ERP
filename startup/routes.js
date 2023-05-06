e61f23239fcb1b101c786ef874ebcf0952f0771b
mohamed El-Raghy <elraghy8@gmail.com>
2023-05-06 16:00:09 +0300

    add main route to salaryhistory

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const employee = require('../routes/employee');
 const task = require('../routes/task');
+const history = require('../routes/salaryHistory');
 
 module.exports = app => {
   app.use('/api/employee', employee);
   app.use('/api/task', task);
+  app.use('/api/history', history);
 }
